f2868070b5f8223ab82b9137ea50e09ddf77ec25
Ruben Götz <ruben.goetz@abgoetz.de>
2026-09-09 13:22:47 +0200

    updated kissat version

--- lib/kissat/fetch-and-build.sh
+++ lib/kissat/fetch-and-build.sh
@@ -3,7 +3,7 @@
 source ../base-build-functions.sh
 dirname="kissat"
 
-branchorcommit="f769a0a5bb91b50b6160d1cb8791cbb0ba1e6312" # updated 2026-07-11
+branchorcommit="5cb25f62e25fede21ac579b898958e58eaf89a82" # updated 2026-07-11
 fetch_and_extract $dirname configure https://github.com/rubenGoetz/kissat/archive/${branchorcommit}.zip
 
 echo "[kissat] Building ..."
