53558ad2d91d9886d3a91c75cdb50136e028c5c8
Ruben Götz <ruben.goetz@abgoetz.de>
2026-09-17 10:52:19 +0200

    fixed kissat link

--- lib/kissat/fetch-and-build.sh
+++ lib/kissat/fetch-and-build.sh
@@ -4,7 +4,7 @@ source ../base-build-functions.sh
 dirname="kissat"
 
 branchorcommit="5cb25f62e25fede21ac579b898958e58eaf89a82" # updated 2026-09-02
-fetch_and_extract $dirname configure https://github.com/domschrei/kissat/archive/${branchorcommit}.zip
+fetch_and_extract $dirname configure https://github.com/rubenGoetz/kissat/archive/${branchorcommit}.zip
 # Niccos Sweep Kissat
 # branchorcommit="d4e76a387c93b28bbe84a2db043008e5bc70b185" #FMCAD26 Artifact Commit
 # fetch_and_extract $dirname configure https://github.com/nrilu/kissat/archive/${branchorcommit}.zip
